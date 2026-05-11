2991a7705b57fb514c3c45a177878c801245bce8
seungwoo <rio970319@gmail.com>
2026-05-11 17:12:33 +0900

    #3 feat: NetworkModule 선언

--- data/src/main/java/com/fourbeat/data/network/di/NetworkModule.kt
+++ data/src/main/java/com/fourbeat/data/network/di/NetworkModule.kt
@@ -1,4 +1,57 @@
 package com.fourbeat.data.network.di
 
-object NetworkModule {
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+    private val networkJson = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+    }
+
+    private fun createKtorClient(): HttpClient =
+        HttpClient(OkHttp) {
+            install(ContentNegotiation) {
+                json(networkJson)
+            }
+            install(Logging) {
+                level = LogLevel.BODY
+            }
+        }
+
+    @Provides
+    @Singleton
+    @DefaultNetwork
+    fun provideDefaultHttpClient(): HttpClient =
+        createKtorClient().config {
+            defaultRequest {
+                url("")
+                contentType(ContentType.Application.Json)
+            }
+        }
+
+    @Provides
+    @Singleton
+    @SpotifyNetwork
+    fun provideSpotifyHttpClient(): HttpClient =
+        createKtorClient().config {
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+            }
+        }
 }
