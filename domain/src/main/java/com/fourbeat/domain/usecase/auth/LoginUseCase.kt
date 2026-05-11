155d1b227bde835c1c289ea59b02b5dcdb175e62
seungwoo <rio970319@gmail.com>
2026-05-11 19:16:13 +0900

    #3 feat: uid datastore 저장 로직 추가

--- domain/src/main/java/com/fourbeat/domain/usecase/auth/LoginUseCase.kt
+++ domain/src/main/java/com/fourbeat/domain/usecase/auth/LoginUseCase.kt
@@ -1,12 +1,15 @@
 package com.fourbeat.domain.usecase.auth
 
 import com.fourbeat.domain.repository.AuthRepository
+import com.fourbeat.domain.repository.PreferenceRepository
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository,
+    private val preferenceRepository: PreferenceRepository,
 ) {
     suspend operator fun invoke(email: String): Result<Unit> = runCatching {
-        val uid = authRepository.login(email)
+        authRepository.login(email)
+            .also { uid -> preferenceRepository.saveUid(uid) }
     }
 }
--- domain/src/main/java/com/fourbeat/domain/usecase/user/RegisterUseCase.kt
+++ domain/src/main/java/com/fourbeat/domain/usecase/user/RegisterUseCase.kt
@@ -1,13 +1,16 @@
 package com.fourbeat.domain.usecase.user
 
 import com.fourbeat.domain.model.user.RegisterRequest
+import com.fourbeat.domain.repository.PreferenceRepository
 import com.fourbeat.domain.repository.UserRepository
 import javax.inject.Inject
 
 class RegisterUseCase @Inject constructor(
     private val userRepository: UserRepository,
+    private val preferenceRepository: PreferenceRepository,
 ) {
     suspend operator fun invoke(request: RegisterRequest): Result<Unit> = runCatching {
-        val uid = userRepository.register(request)
+        userRepository.register(request)
+            .also { uid -> preferenceRepository.saveUid(uid) }
     }
 }
